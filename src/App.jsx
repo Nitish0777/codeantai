2a143d05ce69486dff63051f3f0b1fb79cfea0a7
Nitish Nashine <nitishnashine@gmail.com>
2024-12-21 19:26:45 +0530

    feat: add react-router-dom for routing and update App structure with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Side from './components/sidebar/Side'
-import SignUpPage from './components/SignUp'
-import LoginPage from './components/SignIn'
+import "./App.css";
+import {Routes, Route } from "react-router-dom";
+import Side from "./components/sidebar/Side";
+import SignUpPage from "./components/SignUp";
+import LoginPage from "./components/SignIn";
 
 function App() {
-
   return (
     <>
-      {/* <Side /> */}
-      {/* <LoginPage /> */}
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<Side />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<LoginPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
